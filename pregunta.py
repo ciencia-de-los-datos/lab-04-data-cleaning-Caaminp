cfef938a70e8d297f651cb84dfcb7ed6bb432620
Caaminp <160285915+Caaminp@users.noreply.github.com>
2024-04-24 08:22:09 -0500

    Intento 2

--- pregunta.py
+++ pregunta.py
@@ -11,10 +11,21 @@ import pandas as pd
 
 def clean_data():
 
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
+    df = pd.read_csv("solicitudes_credito.csv", sep=";",index_col=0)
+    df = df.copy()
 
-    #
-    # Inserte su código aquí
-    #
+    df.dropna(inplace=True)
+    
+    df = df.apply(lambda x: x.astype(str))
+    df = df.apply(lambda x: x.str.lower () if x.dtype=="objetc" else x)
+    df = df.apply(lambda x: x.str.replace("$", "")) 
+    df = df.apply(lambda x: x.str.replace(",", "")) 
+    df = df.apply(lambda x: x.str.replace("-", " ") if x.dtype == "object" else x) 
+    df = df.apply(lambda x: x.str.replace("_", " ") if x.dtype == "object" else x)  
+    df = df.apply(lambda x: x.str.replace("  ", " " ) if x.dtype == "object" else x)
 
+    df ['monto_del_credito'] = df['monto_del_credito'].astype(float)
+    df ['fecha_de_beneficio'] = pd.tp_datetime(df['fecha_De_beneficio'], dayfirst=True, format = 'mixed')
+
+    df.drop_duplicates(inplace=True)
     return df
